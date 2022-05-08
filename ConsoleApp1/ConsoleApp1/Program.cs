2047b8a33f7f17f589730a5740939f43d3640ca0
awolferret <104518827+awolferret@users.noreply.github.com>
2022-05-08 17:49:57 +0300

    1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int depthCount = 0;
+            int count = 0;
+
+
+            for (int i = 0; i < text.Length; i++)
+            {
+
+                if (text[i] == '(')
+                {
+                    depthCount++;
+                    count++;
+                }
+                else if ((count > 0 && text[i] == ')'))
+                {
+                    count--;
+                }
+                //else if (text[i] == ')')
+                //{
+                    
+                //}
+            }
+
+            if (count >= 0)
+            {
+                Console.WriteLine("Скобочная последвательность верна");
+                Console.WriteLine($"Глубина {depthCount}");
+            }
+            else
+            {
+                Console.WriteLine("Последовательность не верна");
+            }
+
+            //if (opener > closer)
+            //{
+            //    result = closer;
+            //    Console.WriteLine($"Результат глубины {result}");
+            //}
+
+            //else if (closer > opener)
+            //{
+            //    result = opener;
+            //    Console.WriteLine($"Результат глубины {result}");
+            //}
+
+            //else
+            //{
+            //    result = opener;
+            //    Console.WriteLine($"Результат глубины {result}");
+            //}
+        }
+    }
+}
